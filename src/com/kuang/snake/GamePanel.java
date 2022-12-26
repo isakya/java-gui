f7c893c520617d6931470f2d3331f0298cfa8f5b
konglong <1016361268@qq.com>
2022-12-26 23:56:37 +0800

    积分系统、失败判定

--- src/com/kuang/snake/GamePanel.java
+++ src/com/kuang/snake/GamePanel.java
@@ -22,8 +22,12 @@ public class GamePanel extends JPanel implements KeyListener, ActionListener {
     Random random = new Random();
     // 游戏当前的状态：开始、停止
     boolean isStart = false; // 默认不开始
+    int score; // 成绩
     // 定时器
     Timer timer = new Timer(100, this); // 100毫秒执行一次
+
+    // 游戏是否失败
+    boolean isFail = false; // 默认不失败
 
 
     // 构造器
@@ -45,6 +49,7 @@ public class GamePanel extends JPanel implements KeyListener, ActionListener {
         // 把食物随机分布在界面上
         foodx = 25 + 25*random.nextInt(34);
         foody = 75 + 25*random.nextInt(24);
+        score = 0;
     }
 
     // 绘制面板，游戏中的所有东西，都是用这个画笔来画
@@ -56,8 +61,14 @@ public class GamePanel extends JPanel implements KeyListener, ActionListener {
         this.setBackground(Color.WHITE);
         Data.header.paintIcon(this, g, 25, 11); // 头部广告栏
         g.fillRect(25, 75, 850, 600); // 默认的游戏界面
+        // 画积分
+        g.setColor(Color.ORANGE);
+        g.setFont(new Font("微软雅黑", Font.BOLD, 18));
+        g.drawString("长度 " + length, 750, 35);
+        g.drawString("分数 " + score, 750, 50);
 
-
+        // 食物
+        Data.food.paintIcon(this, g, foodx, foody);
         // 把小蛇画上去
         if (fx.equals("R")) {
             Data.right.paintIcon(this, g, snakeX[0], snakeY[0]); // 蛇头,需要通过方向来判断
@@ -68,13 +79,9 @@ public class GamePanel extends JPanel implements KeyListener, ActionListener {
         }else if(fx.equals("D")) {
             Data.down.paintIcon(this, g, snakeX[0], snakeY[0]); // 蛇头,需要通过方向来判断
         }
-
         for (int i = 1; i < length; i++) {
             Data.body.paintIcon(this, g, snakeX[i], snakeY[i]); // 蛇身体
         }
-
-        // 食物
-        Data.food.paintIcon(this, g, foodx, foody);
 
         // 游戏状态
         if(isStart == false) {
@@ -83,14 +90,28 @@ public class GamePanel extends JPanel implements KeyListener, ActionListener {
             g.setFont(new Font("微软雅黑", Font.BOLD, 40));
             g.drawString("按下空格开始游戏", 300,300);
         }
+
+        if(isFail) {
+            g.setColor(Color.RED);
+            // 设置字体
+            g.setFont(new Font("微软雅黑", Font.BOLD, 40));
+            g.drawString("失败，按下空格重新开始", 300,300);
+        }
     }
 
     // 键盘监听事件
     @Override
     public void keyPressed(KeyEvent e) {
         int keyCode = e.getKeyCode(); // 获得键盘按键
+
+
         if(keyCode == KeyEvent.VK_SPACE) {
-            isStart = !isStart;
+            if(isFail) {
+                isFail = false;
+                init();
+            } else {
+                isStart = !isStart;
+            }
             repaint();
         }
         // 小蛇移动
@@ -108,15 +129,16 @@ public class GamePanel extends JPanel implements KeyListener, ActionListener {
     // 事件监听 --- 需要通过固定时间来刷新，1s=10次
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(isStart) { // 如果游戏是开始状态，就让小蛇动起来！
+        if(isStart && isFail == false) { // 如果游戏是开始状态，就让小蛇动起来！
             // 吃食物
             if(snakeX[0] == foodx && snakeY[0] == foody) {
                 length++; // 身体长度 + 1
                 // 再次随机生成食物
                 foodx = 25 + 25*random.nextInt(34);
                 foody = 75 + 25*random.nextInt(24);
+                // 分数+10
+                score += 10;
             }
-
             // 移动
             for (int i = length - 1; i > 0; i--) {
                 snakeX[i] = snakeX[i-1]; // 向前移动一节
@@ -137,19 +159,19 @@ public class GamePanel extends JPanel implements KeyListener, ActionListener {
                 snakeY[0] = snakeY[0] + 25;
                 if(snakeY[0] > 650) { snakeY[0] = 75; }
             }
-
+            // 失败判断，装到自己算失败
+            for (int i = 1; i < length; i++) {
+                if(snakeX[0] == snakeX[i] && snakeY[0] == snakeY[i]) {
+                    isFail = true;
+                }
+            }
             repaint(); // 重绘页面
         }
         timer.start(); // 定时器开启
     }
 
     @Override
-    public void keyTyped(KeyEvent e) {
-
-    }
-
+    public void keyTyped(KeyEvent e) { }
     @Override
-    public void keyReleased(KeyEvent e) {
-
-    }
+    public void keyReleased(KeyEvent e) { }
 }
