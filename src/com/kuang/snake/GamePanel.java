d8f6fe3dee4c0ad4f5e7a782608eaf85c218d7e5
konglong <1016361268@qq.com>
2022-12-26 23:26:26 +0800

    让小蛇动起来

--- src/com/kuang/snake/GamePanel.java
+++ src/com/kuang/snake/GamePanel.java
@@ -2,11 +2,13 @@ package com.kuang.snake;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 // 游戏的面板
-public class GamePanel extends JPanel implements KeyListener {
+public class GamePanel extends JPanel implements KeyListener, ActionListener {
 
     // 定义蛇的数据结构
     int length; // 蛇的长度
@@ -16,6 +18,9 @@ public class GamePanel extends JPanel implements KeyListener {
     // 游戏当前的状态：开始、停止
     boolean isStart = false; // 默认不开始
 
+    // 定时器
+    Timer timer = new Timer(100, this); // 100毫秒执行一次
+
 
     // 构造器
     public GamePanel() {
@@ -23,6 +28,7 @@ public class GamePanel extends JPanel implements KeyListener {
         // 获得焦点和键盘事件
         this.setFocusable(true); // 获得焦点事件
         this.addKeyListener(this); // 获得键盘监听事件
+        timer.start(); // 游戏一开始、定时器就启动
     }
 
     // 初始化方法
@@ -69,8 +75,6 @@ public class GamePanel extends JPanel implements KeyListener {
         }
     }
 
-
-
     // 键盘监听事件
     @Override
     public void keyPressed(KeyEvent e) {
@@ -79,14 +83,55 @@ public class GamePanel extends JPanel implements KeyListener {
             isStart = !isStart;
             repaint();
         }
+        // 小蛇移动
+        if(keyCode == KeyEvent.VK_UP) {
+            fx = "U";
+        } else if(keyCode == KeyEvent.VK_DOWN) {
+            fx = "D";
+        }else if(keyCode == KeyEvent.VK_LEFT) {
+            fx = "L";
+        }else if(keyCode == KeyEvent.VK_RIGHT) {
+            fx = "R";
+        }
+    }
+
+    // 事件监听 --- 需要通过固定时间来刷新，1s=10次
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(isStart) { // 如果游戏是开始状态，就让小蛇动起来！
+            // 移动
+            for (int i = length - 1; i > 0; i--) {
+                snakeX[i] = snakeX[i-1]; // 向前移动一节
+                snakeY[i] = snakeY[i-1];
+            }
+            // 走向
+            if(fx.equals("R")) {
+                snakeX[0] = snakeX[0] + 25;
+                // 边界判断
+                if(snakeX[0] > 850) { snakeX[0] = 25; }
+            } else if(fx.equals("L")) {
+                snakeX[0] = snakeX[0] - 25;
+                if(snakeX[0] < 25) { snakeX[0] = 850; }
+            } else if(fx.equals("U")) {
+                snakeY[0] = snakeY[0] - 25;
+                if(snakeY[0] < 75) { snakeY[0] = 650; }
+            } else if(fx.equals("D")) {
+                snakeY[0] = snakeY[0] + 25;
+                if(snakeY[0] > 650) { snakeY[0] = 75; }
+            }
+
+            repaint(); // 重绘页面
+        }
+        timer.start(); // 定时器开启
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-
-    }
-    @Override
-    public void keyTyped(KeyEvent e) {
 
     }
 }
